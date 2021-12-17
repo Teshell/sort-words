db196cbd916518433bb282d7b859d1f18d2ea276
Teshell <fayrongeek@gmail.com>
2021-12-17 19:32:27 +0100

    words list show

--- script.js
+++ script.js
@@ -5,12 +5,31 @@ const words_list_element = document.querySelector(".words-list");
 btn_sort.addEventListener("click", () => {
   const words = document.querySelector("textarea").value;
   const array_words = words.split(/[\n,]+/);
-  const word_element = document.createElement("p");
+
+  const lengths = array_words.map(function (word) {
+    return word.length;
+  });
+
+  const lengths_converted = [...new Set(lengths)];
+  words_list_element.innerHTML = "";
 
   if (words) {
-    for (i = 0; i < array_words.length; i++) {
-      console.log(`${array_words[i].length} letters: ${array_words[i]}`);
-    }
+    lengths_converted.forEach((leng) => {
+      const number_element = document.createElement("h2");
+
+      number_element.innerHTML = `Words with ${leng} letters:`;
+
+      words_list_element.appendChild(number_element);
+
+      for (i = 0; i < array_words.length; i++) {
+        if (leng === array_words[i].length) {
+          const words_element = document.createElement("p");
+          console.log(`${array_words[i]}`);
+          words_element.innerHTML = `${array_words[i]}`;
+          words_list_element.appendChild(words_element);
+        }
+      }
+    });
   } else {
     alert("The text field is empty !");
   }
