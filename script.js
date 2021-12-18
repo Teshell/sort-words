95c6357d36acd0045f0c262de9a581cbb987ed15
Teshell <fayrongeek@gmail.com>
2021-12-18 13:23:02 +0100

    added sort words

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const containerElement = document.querySelector(".container");
 const btn_sort = document.querySelector(".btn-sort");
 const words_list_element = document.querySelector(".words-list");
 
@@ -6,15 +5,16 @@ btn_sort.addEventListener("click", () => {
   const words = document.querySelector("textarea").value;
   const array_words = words.split(/[\n,]+/);
 
-  const lengths = array_words.map(function (word) {
+  const lengths = array_words.map((word) => {
     return word.length;
   });
 
   const lengths_converted = [...new Set(lengths)];
+
   words_list_element.innerHTML = "";
 
   if (words) {
-    lengths_converted.forEach((leng) => {
+    lengths_converted.sort().forEach((leng) => {
       const number_element = document.createElement("h2");
 
       number_element.innerHTML = `Words with ${leng} letters:`;
@@ -24,7 +24,6 @@ btn_sort.addEventListener("click", () => {
       for (i = 0; i < array_words.length; i++) {
         if (leng === array_words[i].length) {
           const words_element = document.createElement("p");
-          console.log(`${array_words[i]}`);
           words_element.innerHTML = `${array_words[i]}`;
           words_list_element.appendChild(words_element);
         }
